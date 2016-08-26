ff27992a07bd593e5d68407aa11ad7f1f112ff39
IMAHiji <lukeaskins@gmail.com>
2016-08-26 12:35:13 -0500

    lighten shadows on chat bubbles

--- build/style.35884be313c36ca1f724.js
+++ build/style.35884be313c36ca1f724.js
@@ -1,2 +1,2 @@
 webpackJsonp([1,3],{0:function(n,o,c){n.exports=c(306)},306:function(n,o){}});
-//# sourceMappingURL=style.eff0601a10d387dd31c6.js.map
+//# sourceMappingURL=style.35884be313c36ca1f724.js.map
