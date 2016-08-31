846af5677b9b39e75973ddb2b2a273035d36f7da
IMAHiji <lukeaskins@gmail.com>
2016-08-31 13:42:56 -0500

    update build package

--- build/style.b9523cd0d2c0dfe5006e.js
+++ build/style.b9523cd0d2c0dfe5006e.js
@@ -1,2 +1,2 @@
 webpackJsonp([1,3],{0:function(n,o,c){n.exports=c(418)},418:function(n,o){}});
-//# sourceMappingURL=style.dfb78bb02025d767e3c4.js.map
+//# sourceMappingURL=style.b9523cd0d2c0dfe5006e.js.map
